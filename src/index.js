abc3d45ebb04f45b5bf9a677952b6f5a38d52447
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-01-11 13:00:36 +0200

    added solution final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const arrNumbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const arrDozens = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const arrSingles = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    function calcPart(num) {
+        let a, b, c;
+        newArr = [];
+        let arr = String(num).split("");
+        a = +arr[0];
+        b = +arr[1];
+        c = +arr[2];
+        newArr = [a, b, c];
+
+        return newArr;
+    }
+
+    calcPart(number);
+
+    if (number > 99) {
+        if (newArr[1] > 1) {
+            if (newArr[2]) {
+                return `${arrSingles[newArr[0] - 1]} hundred ${
+                    arrDozens[newArr[1] - 2]
+                } ${arrSingles[newArr[2] - 1]}`;
+            }
+            return `${arrSingles[newArr[0] - 1]} hundred ${
+                arrDozens[newArr[1] - 2]
+            }`;
+        }
+
+        if (newArr[1] === 1) {
+            let digit = +("1" + newArr[2]);
+            return `${arrSingles[newArr[0] - 1]} hundred ${arrNumbers[digit]}`;
+        }
+
+        if (newArr[2]) {
+            return `${arrSingles[newArr[0] - 1]} hundred ${
+                arrSingles[newArr[2] - 1]
+            }`;
+        }
+        return `${arrSingles[newArr[0] - 1]} hundred`;
+    }
+
+    if (number > 19) {
+        if (newArr[1]) {
+            return `${arrDozens[newArr[0] - 2]} ${arrSingles[newArr[1] - 1]}`;
+        }
+        return `${arrDozens[newArr[0] - 2]}`;
+    }
+    return arrNumbers[number];
+};
